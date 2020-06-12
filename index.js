25bf997a2412eb6f476ef67ce2e77587c016b2fe
thomasWillems <thomas.willems.kessel@gmail.com>
2020-06-12 17:18:24 +0200

    first algo done

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var isPalindrome = function (string) {
-    if (string == string.split('').reverse().join('')) {
-        alert(string + ' is a palin');
-    }
-    else{
-        alert(string + 'is not palin');
+paloArray = [];
+for (x=0; x<string.length; x++) {
+    for (y = x + 1; y < string.length; y++) {
+        if (string[x] === string[y]) {
+            let paloString = string.slice(x, y + 1);
+            let stringReverse = paloString.split('').reverse().join('');
+            if (paloString === stringReverse) {
+                paloArray.push(paloString);
+
+            }
+        }
     }
 }
+console.log(paloArray);
